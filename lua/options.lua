8d521097d92580a31df62ad020186510437bdec2
Louca GUERMEUR <louca.guermeur@epita.fr>
2023-05-29 00:47:24 +0200

    added some important options

--- lua/options.lua
+++ lua/options.lua
@@ -4,14 +4,17 @@ vim.g.maplocalleader = ' '
 
 --vim cursor
 vim.opt.guicursor = ''
---vim.opt.cursorline = true
+vim.opt.cursorline = true
 
 --vim line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
 
 --vim color column
 vim.opt.colorcolumn = '80'
+
+vim.opt.termguicolors = true
 
 --vim tabs
 vim.opt.tabstop = 4
@@ -31,14 +34,38 @@ vim.opt.laststatus = 2
 --vim scroll
 vim.opt.scrolloff = 8
 
+--vim search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+
 --vim tmp files
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.writebackup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
+vim.opt.undolevels = 500
 
 --vim mouse disbled
 vim.opt.mouse = ""
+
+--vim completion
+vim.opt.wildignore = '*.o,*.r'
+
+--vim file reading
+vim.opt.fileencoding = 'utf-8'
+
+--vim cmd
+vim.opt.cmdheight = 2
+
+--vim splitwindow
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+--vim sound
+vim.opt.visualbell = true
+vim.opt.errorbells = false
 
 
 --     ########################## 
